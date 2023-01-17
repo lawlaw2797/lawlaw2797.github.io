b4f0a580c6802b8ce4108426ac119e1897c40e25
lawlaw2797 <yurikonanaop2@gmail.com>
2023-01-18 03:31:07 +0700

    Blank Page Fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import BrowserRouter from "react-router-dom/BrowserRouter";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+<BrowserRouter basename={process.env.PUBLIC_URL}>
+<App />
+</BrowserRouter>);
